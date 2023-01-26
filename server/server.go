7be29f64eeb1005d52b3c6c9fac43a6f2995c5d4
DjCtavia <djctavia@gmail.com>
2023-01-26 19:16:10 +0100

    Listening server with "/data" path

--- server/server.go
+++ server/server.go
@@ -1,7 +1,17 @@
 package server
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func RunServer() {
 	fmt.Println("Server is Running on port 8080.")
+	http.HandleFunc("/data", DataHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func DataHandler(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(res, "Hello Github! Welcome on \"%s\" path!", req.URL.Path[1:])
 }
