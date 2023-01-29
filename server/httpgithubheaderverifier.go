ffdc9090cce3be6d8166a5ef8a52f924b2a27aa0
DjCtavia <djctavia@gmail.com>
2023-01-29 20:36:51 +0100

    Preferred method with pointer

--- server/httpgithubheaderverifier.go
+++ server/httpgithubheaderverifier.go
@@ -9,21 +9,21 @@ type GithubHeader struct {
 	hubSignatureSecret256 string
 }
 
-func (gitH GithubHeader) Init(req *http.Request) {
+func (gitH *GithubHeader) Init(req *http.Request) {
 	gitH.httpRequest = req
 	gitH.hubSignatureSecret = req.Header.Get("X-Hub-Signature")
 	gitH.hubSignatureSecret = req.Header.Get("X-Hub-Signature-256")
 	gitH.event = GithubEvent(req.Header.Get("X-GitHub-Event"))
 }
 
-func (gitH GithubHeader) Verify() bool {
+func (gitH *GithubHeader) IsValidHeader() bool {
 	if gitH.isNotContentTypeApplicationJson() {
 		return false
 	}
 	return true
 }
 
-func (gitH GithubHeader) isContentTypeApplicationJson() bool {
+func (gitH *GithubHeader) isContentTypeApplicationJson() bool {
 	contentType := gitH.httpRequest.Header.Get("Content-Type")
 	if contentType == "application/json" {
 		return true
@@ -31,10 +31,10 @@ func (gitH GithubHeader) isContentTypeApplicationJson() bool {
 	return false
 }
 
-func (gitH GithubHeader) isNotContentTypeApplicationJson() bool {
+func (gitH *GithubHeader) isNotContentTypeApplicationJson() bool {
 	return !gitH.isContentTypeApplicationJson()
 }
 
-func (gitH GithubHeader) GetEvent() GithubEvent {
+func (gitH *GithubHeader) GetEvent() GithubEvent {
 	return gitH.event
 }
