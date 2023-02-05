87293d842e17da5f516b91ae24c61bc6e22114bb
DjCtavia <djctavia@gmail.com>
2023-02-05 18:35:00 +0100

    Modified DiscordEmbed struct for including Image and Thumbnail

--- discord/discordembed.go
+++ discord/discordembed.go
@@ -2,17 +2,17 @@ package discord
 
 // https://discord.com/developers/docs/resources/channel#embed-object
 type DiscordEmbed struct {
-	Title       string         `json:"title,omitempty"`
-	Type        string         `json:"type,omitempty"`
-	Description string         `json:"description,omitempty"`
-	Url         string         `json:"url,omitempty"`
-	Timestamp   string         `json:"timestamp,omitempty"`
-	Color       int            `json:"color,omitempty"`
-	Footer      NotImplemented `json:"footer,omitempty"`
-	Image       NotImplemented `json:"image,omitempty"`
-	Thumbnail   NotImplemented `json:"thumbnail,omitempty"`
-	Video       NotImplemented `json:"video,omitempty"`
-	Provider    NotImplemented `json:"provider,omitempty"`
-	Author      NotImplemented `json:"author,omitempty"`
-	Fields      NotImplemented `json:"fields,omitempty"`
+	Title       string            `json:"title,omitempty"`
+	Type        string            `json:"type,omitempty"`
+	Description string            `json:"description,omitempty"`
+	Url         string            `json:"url,omitempty"`
+	Timestamp   string            `json:"timestamp,omitempty"`
+	Color       int               `json:"color,omitempty"`
+	Footer      NotImplemented    `json:"footer,omitempty"`
+	Image       DiscordEmbedImage `json:"image,omitempty"`
+	Thumbnail   DiscordThumbnail  `json:"thumbnail,omitempty"`
+	Video       NotImplemented    `json:"video,omitempty"`
+	Provider    NotImplemented    `json:"provider,omitempty"`
+	Author      NotImplemented    `json:"author,omitempty"`
+	Fields      NotImplemented    `json:"fields,omitempty"`
 }
