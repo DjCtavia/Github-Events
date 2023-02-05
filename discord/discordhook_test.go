0a9c598b10ff174ee68474d93d46ce773d5ed502
DjCtavia <djctavia@gmail.com>
2023-02-05 18:35:29 +0100

    Split test in functions, and added SendMessage with embed test

--- discord/discordhook_test.go
+++ discord/discordhook_test.go
@@ -5,6 +5,11 @@ import "testing"
 func TestDiscordHook_SendMessage(t *testing.T) {
 	dh := DiscordHook{"https://discord.com/api/webhooks/1070751564505038990/3FbPB1qPAWKf_ISUsxyg3ZE-hF-_oyfdGArrdfTGQXuffAvpjWRoE4k69yPsE_mXkiOi"}
 
+	testDiscordHook_SendMessage_content(t, &dh)
+	testDiscordHook_SendMessage_embed(t, &dh)
+}
+
+func testDiscordHook_SendMessage_content(t *testing.T, dh *DiscordHook) {
 	dmessage := DiscordMessage{}
 	err := dh.SendMessage(dmessage)
 	if err == nil {
@@ -14,6 +19,25 @@ func TestDiscordHook_SendMessage(t *testing.T) {
 
 	dmessage = DiscordMessage{Content: "It shoud work!"}
 	err = dh.SendMessage(dmessage)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+}
+
+func testDiscordHook_SendMessage_embed(t *testing.T, dh *DiscordHook) {
+	dthumbnail := DiscordThumbnail{Url: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png", Width: 16, Height: 16}
+	dembed := DiscordEmbed{
+		Type:        "rich",
+		Title:       "Test",
+		Description: "Description test",
+		Color:       HexRGBToInt("6fab54"),
+		Thumbnail:   dthumbnail,
+	}
+	embeds := []DiscordEmbed{dembed}
+	dmessage := DiscordMessage{Embeds: embeds}
+
+	err := dh.SendMessage(dmessage)
 	if err != nil {
 		t.Error(err)
 		return
