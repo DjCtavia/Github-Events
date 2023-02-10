088b898805ae3a8ef62b0dc7ea0e7f556a5621dc
DjCtavia <djctavia@gmail.com>
2023-02-10 01:11:25 +0100

    Update GithubEvent structure and rename the GithubEvent

--- server/githubevent.go
+++ server/githubevent.go
@@ -1,19 +1,57 @@
 package server
 
-type GitHubEvent struct {
-	Action     string `json:"action"`
-	Repository struct {
-		Name     string `json:"name"`
-		FullName string `json:"full_name"`
-		URL      string `json:"html_url"`
-	} `json:"repository"`
-	Sender struct {
-		Login string `json:"login"`
-		URL   string `json:"html_url"`
-	} `json:"sender"`
-	Commit struct {
-		Sha     string `json:"sha"`
-		Message string `json:"message"`
-		Url     string `json:"url"`
-	} `json:"commit"`
+type GithubEvent struct {
+	Action     string
+	Ref        string           `json:"ref"`
+	Before     string           `json:"before"`
+	After      string           `json:"after"`
+	Created    bool             `json:"created"`
+	Deleted    bool             `json:"deleted"`
+	Forced     bool             `json:"forced"`
+	BaseRef    string           `json:"base_ref"`
+	Compare    string           `json:"compare"`
+	Commits    []GithubCommit   `json:"commits"`
+	HeadCommit GithubCommit     `json:"head_commit"`
+	Repository GithubRepository `json:"repository"`
+	Pusher     GithubUser       `json:"pusher"`
+	Sender     GithubUser       `json:"sender"`
+}
+
+type GithubCommit struct {
+	ID        string     `json:"id"`
+	Message   string     `json:"message"`
+	Timestamp string     `json:"timestamp"`
+	URL       string     `json:"url"`
+	Author    GithubUser `json:"author"`
+	Committer GithubUser `json:"committer"`
+}
+
+type GithubRepository struct {
+	ID       int        `json:"id"`
+	NodeID   string     `json:"node_id"`
+	Name     string     `json:"name"`
+	FullName string     `json:"full_name"`
+	Private  bool       `json:"private"`
+	Owner    GithubUser `json:"owner"`
+}
+
+type GithubUser struct {
+	Login             string `json:"login"`
+	ID                int    `json:"id"`
+	NodeID            string `json:"node_id"`
+	AvatarURL         string `json:"avatar_url"`
+	GravatarID        string `json:"gravatar_id"`
+	URL               string `json:"url"`
+	HTMLURL           string `json:"html_url"`
+	FollowersURL      string `json:"followers_url"`
+	FollowingURL      string `json:"following_url"`
+	GistsURL          string `json:"gists_url"`
+	StarredURL        string `json:"starred_url"`
+	SubscriptionsURL  string `json:"subscriptions_url"`
+	OrganizationsURL  string `json:"organizations_url"`
+	ReposURL          string `json:"repos_url"`
+	EventsURL         string `json:"events_url"`
+	ReceivedEventsURL string `json:"received_events_url"`
+	Type              string `json:"type"`
+	SiteAdmin         bool   `json:"site_admin"`
 }
