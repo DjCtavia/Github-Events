8872a0cf7f0077eb5124b591fc07001e758ef638
DjCtavia <djctavia@gmail.com>
2023-02-10 01:50:52 +0100

    Update main function

--- main.go
+++ main.go
@@ -3,10 +3,21 @@ package main
 import (
 	"djctavia/server"
 	"fmt"
+	"observer"
 )
 
 func main() {
 	fmt.Println("GithubEvents Started.")
-	serv := server.Server{Port: server.DEFAULTSERVERPORT}
+
+	// Example
+	// observer.GlobalSubject.Register(&observer.GithubObserver{Id: "Main Discord Server", Hook: &discord.DiscordHook{Url: "https://discord.com/api/webhooks/7777777777777777777/abcdefg_abcd_abcdefg012345-abcedfg124567987a_bcedf_abcedf01234456789"}})
+
+	serv := server.GetServer()
+	serv.BindPort(server.DEFAULTSERVERPORT)
+	serv.BindNotifyFunc(NotifyEvent)
 	serv.RunServer()
+}
+
+func NotifyEvent(event *server.GithubEvent) {
+	go observer.GlobalSubject.Notify(event)
 }
